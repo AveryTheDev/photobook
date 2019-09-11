2f4a237decbdc82e4e9fde1e31de99aa70895b8e
abrown945 <abrown945@gmail.com>
2019-09-10 20:04:28 -0400

    refactor with hooks

--- src/components/ImageSearch/ImageCard.js
+++ src/components/ImageSearch/ImageCard.js
@@ -1,63 +1,44 @@
-import React, { Component } from 'react';
-import { PhotoContext } from '../../context/PhotoContext';
+import React, { useState, useLayoutEffect, useContext, useRef } from "react";
+import { PhotoContext } from "../../context/PhotoContext";
 
-class ImageCard extends Component {
-    state={spans: 0}
+const ImageCard = ({ image, description }) => {
+  const { setSelected } = useContext(PhotoContext);
+  const [spans, setSpans] = useState(0);
 
-    componentDidMount() {
-        this.imageRef.current.addEventListener('load', this.setSpans);
-    }
+  const imageRef = useRef();
 
-    setSpans = () => {
+useLayoutEffect(() => {
+  if (imageRef.current) {
+    const setImageSpans = () => {
+      const height = imageRef.current.clientHeight;
+      const imgSpans = Math.ceil(height / 40 + 1);
+      setSpans(imgSpans);
+    };
+    imageRef.current.addEventListener("load", setImageSpans);
+    return () => imageRef.current.removeEventListener("load", setImageSpans);
+  }
+}, [imageRef.current]);
 
-        const height = this.imageRef.current.clientHeight;
-        const spans = Math.ceil(height / 40 + 1);
-        this.setState({ spans: spans });
-    }
+  const imageSelect = () => {
+    setSelected({
+      image: image.urls.regular,
+      description: description,
+      downloadURL: image.links.download,
+      photographer: `${image.user.first_name} ${image.user.last_name}`,
+      portfolioURL: image.user.portfolio_url
+    });
+  };
 
-        static contextType = PhotoContext;
+  return (
+    <div style={{ gridRowEnd: `span ${spans}` }}>
+      <img
+        ref={imageRef}
+        alt={description}
+        src={image.urls.regular}
+        onClick={imageSelect}
+      />
+    </div>
+  );
+};
 
-    imageSelect = () => {
-
-    let last_name = this.props.image.user.last_name;
-
-        const setLastName = () => {
-            if(last_name) {
-                return;
-            }
-            last_name = '';
-        }
-
-            setLastName();
-
-    this.context.setSelected({
-          image: this.props.image.urls.regular,
-          description: this.props.description,
-          downloadURL: this.props.image.links.download,
-          photographer: `${this.props.image.user.first_name} ${last_name}`,
-          portfolioURL: this.props.image.user.portfolio_url
-        });
-    }
-
-    render() { 
-
-    this.imageRef = React.createRef();
-
-    const { image, description } = this.props;
-
-
-        return (
-          <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
-            <img
-              ref={this.imageRef}
-              alt={description}
-              src={image.urls.regular}
-              onClick={this.imageSelect}
-            />
-          </div>
-        );
-    }
-}
- 
 export default ImageCard;
-
