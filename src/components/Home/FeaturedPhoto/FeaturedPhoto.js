089f159f05730d68eb7770b8ead9f055824b6f8b
abrown945 <abrown945@gmail.com>
2019-11-01 17:26:13 -0400

    new photo

--- src/components/Home/FeaturedPhoto/FeaturedPhoto.js
+++ src/components/Home/FeaturedPhoto/FeaturedPhoto.js
@@ -1,41 +1,37 @@
-import './FeaturedPhoto.css';
-import unsplash from '../../../api/unsplash';
-import React from 'react';
-import Image from 'react-bootstrap/Image';
-import HomePhoto from '../../../images/photobook.jpg';
-import SearchBar from '../../ImageSearch/SearchBar/SearchBar';
-import ImageList from '../../ImageSearch/ImageList';
-
-
+import "./FeaturedPhoto.css";
+import unsplash from "../../../api/unsplash";
+import React from "react";
+import Image from "react-bootstrap/Image";
+import HomePhoto from "../../../images/frontpage.jpg";
+import SearchBar from "../../ImageSearch/SearchBar/SearchBar";
+import ImageList from "../../ImageSearch/ImageList";
 
 class FeaturedPhoto extends React.Component {
-    state = { images: [] }
+  state = { images: [] };
 
-    onSearchSubmit = async (term) => {
-        const response = await unsplash.get('/search/photos', {
-            params: { query: term, per_page: 50 },
-        });
+  onSearchSubmit = async term => {
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term, per_page: 50 }
+    });
 
-        this.setState({ images: response.data.results });
-    }
+    this.setState({ images: response.data.results });
+  };
 
-    render() {
-
-        return (
-            <div>
-                <div className="featuredPhotoContainer">
-                    <Image src={HomePhoto} className="highlightPhoto" />
-                    <div className="imageContent">
-                        <SearchBar userSubmit={this.onSearchSubmit}/>
-                    </div>
-                </div>
-                <div className="container">
-                    <ImageList images={this.state.images} />
-                </div>
-            </div>
-
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div className="featuredPhotoContainer">
+          <Image src={HomePhoto} className="highlightPhoto" />
+          <div className="imageContent">
+            <SearchBar userSubmit={this.onSearchSubmit} />
+          </div>
+        </div>
+        <div className="container">
+          <ImageList images={this.state.images} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default FeaturedPhoto;
