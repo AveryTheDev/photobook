48e76d7360d6eb2b7dff663d6e6b61ade7f441f0
abrown945 <abrown945@gmail.com>
2019-11-01 22:18:40 -0400

    fix img path

--- src/components/Home/PhotoSearch/PhotoSearch.js
+++ src/components/Home/PhotoSearch/PhotoSearch.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import './PhotoSearch.css';
-import Image from 'react-bootstrap/Image';
-import HomePhoto from '../../../images/photobook.jpg';
-import SearchBar from '../../ImageSearch/SearchBar/SearchBar';
-import ImageList from '../../ImageSearch/ImageList';
+import React from "react";
+import "./PhotoSearch.css";
+import Image from "react-bootstrap/Image";
+import HomePhoto from "../../../images/frontpage.jpg";
+import SearchBar from "../../ImageSearch/SearchBar/SearchBar";
+import ImageList from "../../ImageSearch/ImageList";
 
 const PhotoSearch = () => {
-
-    return ( 
-        <div>
-            <div className="photoContainer">
-                <Image src={HomePhoto} className="highlightPhoto" />
-                <div className="imageContent">
-                    <SearchBar/>
-                </div>
-            </div>
-            <div className="container">
-                <ImageList/>
-            </div>
+  return (
+    <div>
+      <div className="photoContainer">
+        <Image src={HomePhoto} className="highlightPhoto" />
+        <div className="imageContent">
+          <SearchBar />
         </div>
-     );
-}
- 
+      </div>
+      <div className="container">
+        <ImageList />
+      </div>
+    </div>
+  );
+};
+
 export default PhotoSearch;
